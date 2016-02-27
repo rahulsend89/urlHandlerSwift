2f6ed6cb431f84e31dc319803a62a827706796b1
Rahul Malik <rahul.send89@gmail.com>
2016-02-27 14:15:01 +0530

    added support for swift 2.0

--- urlHandlerSwift/UrlHandler.swift
+++ urlHandlerSwift/UrlHandler.swift
@@ -11,10 +11,10 @@ import UIKit
 import SystemConfiguration
 
 var REQUESTTIMEOUT:NSTimeInterval = 10;
-var _completionHandler:((error:NSError?,returnObject:String)->Void)?
+var _completionHandler:((error:ErrorType?,returnObject:String)->Void)?
 var _progressHandler:((Float)->Void)?
 
-var _multiCompletionHandler:((error:NSError?,returnObject:String,current:Int)->Void)?
+var _multiCompletionHandler:((error:ErrorType?,returnObject:String,current:Int)->Void)?
 var _multiProgressHandler:((pre:Float,current:Int)->Void)?
 var _currentVal:Int = 0
 
@@ -61,41 +61,42 @@ class UrlHandler:NSObject{
         return Static.instance!
     }
     private func makeError(text:String="")->NSError{
-        var code:Int = 404;
+        let code:Int = 404;
         return NSError(domain: "HTTPTask", code: code, userInfo: [NSLocalizedDescriptionKey: text])
     }
     func initCache(){
-        var URLCache:NSURLCache = NSURLCache(memoryCapacity: 4 * 1024 * 1024, diskCapacity: 4 * 1024 * 1024, diskPath: nil)
+        let URLCache:NSURLCache = NSURLCache(memoryCapacity: 4 * 1024 * 1024, diskCapacity: 4 * 1024 * 1024, diskPath: nil)
         NSURLCache.setSharedURLCache(URLCache)
+    }
+    func getDocumentsDirectory() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
     }
     func pathValueWithName(filename:NSString,pathName:NSString)->NSString{
         var documentsDirectory:NSString = "";
         if pathName == "doc" {
-            documentsDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0].stringByAppendingPathComponent(filename)
+            documentsDirectory = "\(getDocumentsDirectory())\(filename)"
         }else if pathName == "temp" {
-            documentsDirectory = (NSTemporaryDirectory().stringByAppendingPathExtension(filename))!
+            documentsDirectory = "\(NSTemporaryDirectory())\(filename)"
         }
         return documentsDirectory;
     }
     
     func isConnectedToNetwork() -> Bool {
-        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        var zeroAddress = sockaddr_in()
         zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
         zeroAddress.sin_family = sa_family_t(AF_INET)
-        
         let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
-            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0)).takeRetainedValue()
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
         }
-        
-        var flags: SCNetworkReachabilityFlags = 0
-        if SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) == 0 {
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
             return false
         }
-        
-        let isReachable = (flags & UInt32(kSCNetworkFlagsReachable)) != 0
-        let needsConnection = (flags & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
-        
-        return (isReachable && !needsConnection) ? true : false
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
     }
     
     func basicURL (myURL:String, handler:(error:NSError?,returnObject:String)->Void)->Void{
@@ -103,23 +104,21 @@ class UrlHandler:NSObject{
             let mycurrentURL = NSURL(string: myURL);
             let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
             dispatch_async(dispatch_get_global_queue(priority, 0)) {
-                var request:NSMutableURLRequest = NSMutableURLRequest(URL:mycurrentURL! ,cachePolicy: NSURLRequestCachePolicy.ReloadRevalidatingCacheData,
+                let request:NSMutableURLRequest = NSMutableURLRequest(URL:mycurrentURL! ,cachePolicy: NSURLRequestCachePolicy.ReloadRevalidatingCacheData,
                     timeoutInterval: REQUESTTIMEOUT);
-                var cachedResponse = NSURLCache.sharedURLCache().cachedResponseForRequest(request);
+                let cachedResponse = NSURLCache.sharedURLCache().cachedResponseForRequest(request);
                 if cachedResponse != nil {
-                    var returndata:NSData = cachedResponse!.data;
-                    var returnString = String(NSString(data: returndata, encoding: NSUTF8StringEncoding)!)
+                    let returndata:NSData = cachedResponse!.data;
+                    let returnString = String(NSString(data: returndata, encoding: NSUTF8StringEncoding)!)
                     handler(error: nil,returnObject: returnString);
                 }else{
-                    var reponse:AutoreleasingUnsafeMutablePointer<NSURLResponse?> = nil;
-                    var error: NSError? = nil
                     let queue:NSOperationQueue = NSOperationQueue()
                     NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler: { (reponse, data, error) -> Void in
                         if error == nil{
-                            var _cachedResponse:NSCachedURLResponse? = NSCachedURLResponse(response: reponse, data: data)
+                            let _cachedResponse:NSCachedURLResponse? = NSCachedURLResponse(response: reponse!, data: data!)
                             NSURLCache.sharedURLCache().storeCachedResponse(_cachedResponse!, forRequest: request)
-                            var string:NSString = NSString(data: data, encoding: NSUTF8StringEncoding)!
-                            handler(error: error,returnObject: string);
+                            let string:NSString = NSString(data: data!, encoding: NSUTF8StringEncoding)!
+                            handler(error: error,returnObject: string as String);
                         }else{
                             handler(error:error,returnObject:"notReachable")
                         }
@@ -128,11 +127,11 @@ class UrlHandler:NSObject{
                 }
             }
         }else{
-            handler(error:self.makeError(text: "notReachable"),returnObject:"")
+            handler(error:self.makeError("notReachable"),returnObject:"")
         }
     }
     
-    func downloadFileWithURL(myURL:String,progress:(pre:Float)->Void,handler:(error:NSError?,returnObject:String)->Void)->Void{
+    func downloadFileWithURL(myURL:String,progress:(pre:Float)->Void,handler:(error:ErrorType?,returnObject:String)->Void)->Void{
         _completionHandler = handler
         _progressHandler = progress
         if isConnectedToNetwork() {
@@ -142,13 +141,13 @@ class UrlHandler:NSObject{
                 self.downloading = true;
                 self.expectedContentLength = -1;
                 self.progressContentLength = 0;
-                var filePath:NSString = self.pathValueWithName(myURL.lastPathComponent, pathName: "doc")
+                let filePath:NSString = self.pathValueWithName((mycurrentURL?.lastPathComponent)!, pathName: "doc")
                 self.mainfilename = filePath
-                self.downloadStream = NSOutputStream(toFileAtPath: filePath, append: false)
-                var request:NSMutableURLRequest = NSMutableURLRequest(URL:mycurrentURL! ,cachePolicy: NSURLRequestCachePolicy.ReloadRevalidatingCacheData,
+                self.downloadStream = NSOutputStream(toFileAtPath: filePath as String, append: false)
+                let request:NSMutableURLRequest = NSMutableURLRequest(URL:mycurrentURL! ,cachePolicy: NSURLRequestCachePolicy.ReloadRevalidatingCacheData,
                     timeoutInterval: REQUESTTIMEOUT);
                 if self.downloadStream == nil {
-                    _completionHandler!(error:self.makeError(text: "Cannot create downloadStream"),returnObject:"")
+                    _completionHandler!(error:self.makeError("Cannot create downloadStream"),returnObject:"")
                     self.downloadCompleted(false)
                     return;
                 }
@@ -157,38 +156,38 @@ class UrlHandler:NSObject{
                 self.connection?.scheduleInRunLoop(NSRunLoop.mainRunLoop(), forMode: NSRunLoopCommonModes)
                 self.connection?.start()
                 if self.connection == nil {
-                    _completionHandler!(error:self.makeError(text: "Cannot create connection"),returnObject:"")
+                    _completionHandler!(error:self.makeError("Cannot create connection"),returnObject:"")
                     self.downloadCompleted(false)
                     return;
                 }
             }
         }else{
-            _completionHandler!(error:self.makeError(text: "notReachable"),returnObject:"")
+            _completionHandler!(error:self.makeError("notReachable"),returnObject:"")
         }
     }
     
-    func downloadListOfListWithArray(fileList:NSArray,progress:(pre:Float,current:Int)->Void,handler:(error:NSError?,returnObject:String,current:Int)->Void)->Void{
+    func downloadListOfListWithArray(fileList:NSArray,progress:(pre:Float,current:Int)->Void,handler:(error:ErrorType?,returnObject:String,current:Int)->Void)->Void{
         if fileList.count==0 {
             return;
         }
         _multiCompletionHandler = handler
         _multiProgressHandler = progress
         if isConnectedToNetwork() {
-            var myURL:String = fileList[0] as String;
-            var tempURL:NSURL = NSURL(string: myURL)!
-            var filename:NSString = tempURL.lastPathComponent
+            let myURL:String = fileList[0] as! String;
+//            let tempURL:NSURL = NSURL(string: myURL)!
+//            let filename:NSString = tempURL.lastPathComponent!
             UrlHandler.sharedInstance.downloadFileWithURL(myURL, progress: { (pre) -> Void in
                 _multiProgressHandler!(pre: pre,current: _currentVal)
                 }, handler: { (error, returnObject) -> Void in
                     _multiCompletionHandler!(error: error, returnObject: returnObject, current: _currentVal)
                     _currentVal++;
                     
-                    var array:NSMutableArray = NSMutableArray(array: fileList,copyItems:true)
+                    let array:NSMutableArray = NSMutableArray(array: fileList as [AnyObject],copyItems:true)
                     array.removeObjectAtIndex(0)
                     self.downloadListOfListWithArray((array as NSArray), progress: _multiProgressHandler!, handler: _multiCompletionHandler!)
             })
         }else{
-            _multiCompletionHandler!(error:NSError(),returnObject:"notReachable",current: 0)
+            _multiCompletionHandler!(error:makeError("notReachable"),returnObject:"notReachable",current: 0)
         }
     }
 
@@ -196,8 +195,7 @@ class UrlHandler:NSObject{
         
     }
     func downloadCompleted(val:Bool)->Void{
-        var fileManager:NSFileManager = NSFileManager.defaultManager()
-        var errorVal:NSError?;
+        let fileManager:NSFileManager = NSFileManager.defaultManager()
         if self.connection != nil {
             if !val {
                 self.connection?.cancel()
@@ -210,19 +208,23 @@ class UrlHandler:NSObject{
         }
         self.downloading = false
         if val {
-            if fileManager.fileExistsAtPath(self.mainfilename!) {
-                fileManager.removeItemAtPath(self.mainfilename!, error: &errorVal)
-                if errorVal != nil {
-                    _completionHandler!(error:errorVal,returnObject:"removeItemAtPath _ downloadFailed")
-                    return;
+            if fileManager.fileExistsAtPath(self.mainfilename! as String) {
+                do {
+                    try fileManager.removeItemAtPath(self.mainfilename! as String)
+                }catch{
+                    _completionHandler!(error:error,returnObject:"removeItemAtPath _ downloadFailed")
                 }
             }
-            _completionHandler!(error:errorVal,returnObject:self.mainfilename!)
+            _completionHandler!(error:nil,returnObject:self.mainfilename! as String)
         }else{
             if self.mainfilename != nil {
-                if fileManager.fileExistsAtPath(self.mainfilename!) {
-                    fileManager.removeItemAtPath(self.mainfilename!, error: &errorVal)
-                    _completionHandler!(error:errorVal,returnObject:"ConnectionFailed")
+                if fileManager.fileExistsAtPath(self.mainfilename as! String) {
+                    do {
+                        try fileManager.removeItemAtPath(self.mainfilename! as String)
+                    }catch{
+                        _completionHandler!(error:error,returnObject:"ConnectionFailed")
+                    }
+                    
                 }
             }
         }
@@ -231,11 +233,11 @@ class UrlHandler:NSObject{
     
     func connection(connection: NSURLConnection, didReceiveResponse response: NSURLResponse){
         if response .isKindOfClass(NSHTTPURLResponse .classForCoder()) {
-            var statusCode:NSInteger = (response as NSHTTPURLResponse).statusCode
+            let statusCode:NSInteger = (response as! NSHTTPURLResponse).statusCode
             if statusCode == 200 {
                 self.expectedContentLength = response.expectedContentLength
             }else if statusCode >= 400 {
-                _completionHandler!(error:NSError(),returnObject:"bad HTTP response status code")
+                _completionHandler!(error:nil,returnObject:"bad HTTP response status code")
                 self.downloadCompleted(false)
             }
         }else{
@@ -247,7 +249,7 @@ class UrlHandler:NSObject{
         if !self.downloading {
             return;
         }
-        var datawritten = self.downloadStream?.writeData(data)
+        let datawritten = self.downloadStream?.writeData(data)
         if datawritten == -1{
             self.downloadCompleted(false)
         }
@@ -259,9 +261,9 @@ class UrlHandler:NSObject{
     
     func connectionDidFinishLoading(connection: NSURLConnection){
         if (self.mainfilename != nil) {
-            _completionHandler!(error:NSError(),returnObject:self.mainfilename!);
+            _completionHandler!(error:nil,returnObject:self.mainfilename! as String);
         }else{
-            _completionHandler!(error:NSError(),returnObject:"uploadingCompleted");
+            _completionHandler!(error:nil,returnObject:"uploadingCompleted");
         }
     }
     func connection(connection: NSURLConnection, didFailWithError error: NSError){
